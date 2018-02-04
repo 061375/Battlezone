0e8f5fa71c13d51ca4b49ca3cd9979ad18ce8dfe
Jeremy <j.heminger@061375.com>
2018-02-04 14:19:17 -0500

    support dev polygon

--- js/_dev.js
+++ js/_dev.js
@@ -3,13 +3,29 @@ var Dev = function(o) {
     this.x = o.x;
     this.y = o.y;
     this.r = o.r;
-    this.color = GREEN
+    this.mode = o.mode;
+    this.polya = o.polya;
+    this.color = GREEN;
 }
 Dev.prototype.loop = function() {
+    switch(this.mode) {
+        case 'poly':
+            this.poly();
+            break;
+        default:
+            this.circle();
+    }
+}
+Dev.prototype.circle = function() {
     $w.canvas.circle(this.i,this.x,this.y,this.r,this.color,0.5);
-    //$w.canvas.clear(this.i);
+}
+Dev.prototype.poly = function() {
+    $w.canvas.polygon(this.i,this.polya,this.color,'fill',this.color,0.5);
 }
 
+/**
+ * if DEVMODE this logs various variables to the screen
+ * */
 var Devlog = (function(){
     
     'use strict';
