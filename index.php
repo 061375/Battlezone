8ffb9530c12827a91dc8524a80b0115cae355ef6
Jeremy <j.heminger@061375.com>
2018-02-06 19:56:20 -0500

    og data

--- index.php
+++ index.php
@@ -15,37 +15,39 @@ $dev = isset($_GET['dev']) ? strtotime('now') : false;
 <!DOCTYPE html>
 
 <html>
-<head>
-    <title>Battlezone Remake Built in Javascript By Jeremy Heminger</title>
-    <meta name="description" content="Remake of the 1980 original tank game from Atari built in Javascript using the Wes Mantooth Game Engine" />
-    <meta name="keywords" content="battlezone,classic,arcade,game,tank,battle,3d,war,alien,atari,jeremy,aaron,heminger,wes mantooth,javascript,programming,free" />
-    <link href="css/style.css?<?php echo $dev; ?>" rel="stylesheet" />
+    <head>
+        <title>Battlezone Remake Built in Javascript By Jeremy Heminger</title>
+        <meta name="description" property="og:description" content="Remake of the 1980 original tank game from Atari built in Javascript using the Wes Mantooth Game Engine" />
+        <meta name="keywords" content="battlezone,classic,arcade,game,tank,battle,3d,war,alien,atari,jeremy,aaron,heminger,wes mantooth,javascript,programming,free" />
+        <meta property="og:title" content="Classic Battlezone in Javascript | Jeremy Aaron Heminger" />
+        <meta property="og:image" content="images/og-screen.jpg" />
+        <link href="css/style.css?<?php echo $dev; ?>" rel="stylesheet" />
+        <?php
+        if(false !== $dev) {$devmode = 'true';}else{$devmode='false';}
+        ?>
+        <script>const DEVMODE = <?php echo $devmode; ?></script>
+    </head>
+    <body>
+        <?php if(false !== $dev) { ?><pre id="dev-window"></pre><?php } ?>
+        <div class="game-containers">
+            <div id="dev"></div>
+            <div id="gui"></div>
+            <div id="game"></div>
+            <div id="horizon"></div>
+        </div>
+    <script src="js/wes.mantooth.js?<?php echo $dev; ?>"></script>
     <?php
-    if(false !== $dev) {$devmode = 'true';}else{$devmode='false';}
+        loadsscripts('js/models/');
+        loadsscripts('js/');
+        function loadsscripts($dir) {
+            global $dev;
+            // load files
+            $files = scandir($dir);
+            foreach($files as $file) {
+                if($file == 'wes.mantooth.js' || (strpos($file,'.js') === false)) {continue;}
+                echo '<script src="'.$dir.$file.'?'.$dev.'"></script>'."\n";
+            }   
+        }
     ?>
-    <script>const DEVMODE = <?php echo $devmode; ?></script>
-</head>
-<body>
-    <?php if(false !== $dev) { ?><pre id="dev-window"></pre><?php } ?>
-    <div class="game-containers">
-        <div id="dev"></div>
-        <div id="gui"></div>
-        <div id="game"></div>
-        <div id="horizon"></div>
-    </div>
-<script src="js/wes.mantooth.js?<?php echo $dev; ?>"></script>
-<?php
-    loadsscripts('js/models/');
-    loadsscripts('js/');
-    function loadsscripts($dir) {
-        global $dev;
-        // load files
-        $files = scandir($dir);
-        foreach($files as $file) {
-            if($file == 'wes.mantooth.js' || (strpos($file,'.js') === false)) {continue;}
-            echo '<script src="'.$dir.$file.'?'.$dev.'"></script>'."\n";
-        }   
-    }
-?>
-</body>
+    </body>
 </html>
